3463a4071b7b3c0543fd28952c09d442d1feae61
Tolomaus <niek.bartholomeus@gmail.com>
2013-04-09 20:03:21 +0200

    Included an nunit test

---
+++ InstallationTest.UnitTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace InstallationTest.UnitTest
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+    }
+}
