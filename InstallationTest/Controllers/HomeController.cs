f9bb84ad97ef5f47c62a6946d86ece76f2c44086
Tolomaus <niek.bartholomeus@gmail.com>
2013-05-21 12:59:08 +0200

    #IT-4 Fixed
    Watch out need refactoring

--- InstallationTest/Controllers/HomeController.cs
+++ InstallationTest/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace InstallationTest.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC! - cosmetics reworked step 1";
+            ViewBag.Message = "Welcome to ASP.NET MVC! - cosmetics reworked finished";
             ViewBag.Setting = ConfigurationManager.AppSettings["SomeSetting"];
 
             return View();
