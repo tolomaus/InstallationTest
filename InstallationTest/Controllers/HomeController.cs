94dd2678b44576b3461ea901125cbf9ea27d4fd2
Tolomaus <niek.bartholomeus@gmail.com>
2013-07-13 22:43:59 +0200

    Corrected the bug 1

--- InstallationTest/Controllers/HomeController.cs
+++ InstallationTest/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace InstallationTest.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC! - cosmetics reworked finished";
+            ViewBag.Message = "Welcome to ASP.NET MVC! - cosmetics reworked finished 1";
             ViewBag.Setting = ConfigurationManager.AppSettings["SomeSetting"];
 
             return View();
